9e78687ae6342eb18e537bab371e63d0c10e99cf
lhk3337 <lhk3337@naver.com>
2020-07-23 16:01:36 +0900

    I added map.js and included app_header, app__stats and <Map/> in app__left

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { MenuItem, FormControl, Select } from "@material-ui/core";
 import InfoBox from "./InfoBox";
+import Map from "./Map";
 import "./App.css";
 
 function App() {
@@ -40,35 +41,42 @@ function App() {
 
   return (
     <div className="app">
-      <div className="app__header">
-        <h1>Covid-19 Tracker</h1>
-        <FormControl className="app__dropdown">
-          {/* Title + Select input drowdown field */}
-          <Select variant="outlined" onChange={onCountryChange} value={country}>
-            {/* //다시 확인/ */}
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            {/* Loop through all the countries
+      <div className="app__left">
+        <div className="app__header">
+          <h1>Covid-19 Tracker</h1>
+          <FormControl className="app__dropdown">
+            {/* Title + Select input drowdown field */}
+            <Select
+              variant="outlined"
+              onChange={onCountryChange}
+              value={country}
+            >
+              {/* //다시 확인/ */}
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {/* Loop through all the countries
             and show a drop down
             list of the options */}
-            {countries.map((country) => (
-              <MenuItem value={country.value}>{country.name}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-      </div>
-      <div className="app__stats">
-        <InfoBox title="누적확진자" cases={123} total={3232} />
-        <InfoBox title="완치자" cases={11} total={2222} />
-        <InfoBox title="사망자" cases={41} total={4000} />
-        {/* InfoBoxs title="Coronavirus cases"*/}
-        {/* InfoBoxs title="Coronavirus recoveries"*/}
-        {/* InfoBoxs */}
-      </div>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+        <div className="app__stats">
+          <InfoBox title="누적확진자" cases={123} total={3232} />
+          <InfoBox title="완치자" cases={11} total={2222} />
+          <InfoBox title="사망자" cases={41} total={4000} />
+          {/* InfoBoxs title="Coronavirus cases"*/}
+          {/* InfoBoxs title="Coronavirus recoveries"*/}
+          {/* InfoBoxs */}
+        </div>
 
-      {/* Table */}
-      {/* Graph */}
+        {/* Table */}
+        {/* Graph */}
 
-      {/* Map */}
+        {/* Map */}
+        <Map />
+      </div>
     </div>
   );
 }
