67700c8fc5a9f2a88bb79f33715dfc27241efd3a
lhk3337 <lhk3337@naver.com>
2020-07-29 10:39:12 +0900

    Add Corona Virus Mark to Map

--- src/util.js
+++ src/util.js
@@ -1,4 +1,41 @@
+import React from "react";
+import numeral from "numeral";
+import { Circle, Popup } from "react-leaflet";
+
+const casesTypeColors = {
+  cases: {
+    hex: "#cc1034",
+    multiplier: 800,
+  },
+  recovered: {
+    hex: "#7dd71d",
+    multiplier: 1200,
+  },
+  deaths: {
+    hex: "#fb4443",
+    multiplier: 2000,
+  },
+};
+
 export const sortData = (data) => {
   const sortedData = [...data];
   return sortedData.sort((a, b) => (a.cases > b.cases ? -1 : 1));
 };
+
+//DRAW circles on the map with interactive tooltop
+export const showDataOnMap = (data, casesType = "cases") =>
+  data.map((country) => (
+    <Circle
+      center={[country.countryInfo.lat, country.countryInfo.long]}
+      fillOpacity={0.4}
+      color={casesTypeColors[casesType].hex}
+      fillColor={casesTypeColors[casesType].hex}
+      radius={
+        Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
+      }
+    >
+      <Popup>
+        <h1>IM a popup</h1>
+      </Popup>
+    </Circle>
+  ));
